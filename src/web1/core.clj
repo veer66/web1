1441a225d453d1425f2f9bb158d531bfb44acaf1
Vee Satayamas <vsatayamas@gmail.com>
2019-09-29 19:33:38 +0700

    ep2

--- src/web1/core.clj
+++ src/web1/core.clj
@@ -1,11 +1,20 @@
 (ns web1.core
+  (:require [reitit.core :as r]
+            [reitit.ring :as ring])
   (:gen-class))
 
-(defn app [request]
-  {:status 200
-   :body "@@@@@@@@@@@@@@@@@@ Hi @@@@@@@@@@@@@@@@@"})
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn home [req]
+  {:status 200
+   :body "HOME"})
+
+(defn toto [req]
+  {:status 200
+   :body "TOTO"})
+
+(def router (ring/router [["/" home]
+                          ["/toto" toto]]))
+
+(comment (r/match-by-path router "/toto"))
+
+(def app (ring/ring-handler router))
